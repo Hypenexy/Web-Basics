fa6af0b16f6dd365a4cbc873713c2683db8ce459
Midelight <61563710+Hypenexy@users.noreply.github.com>
2021-08-13 00:54:41 +0300

    JSON Stuff!
    I love json so much.

---
+++ Javascript/Parsing JSON.js
@@ -0,0 +1,5 @@
+const parsedinfo = JSON.parse(info);
+console.log(parsedinfo.desc);
+
+//This takes the json that's stored in info
+//and then outputs the desc part of the json.
---
+++ php/Creating JSON.php
@@ -0,0 +1,9 @@
+<?php
+$datatosend = (object) [];
+$datatosend->version = "2.0.0";
+$datatosend->name = "Hypenexy";
+
+echo json_encode($datatosend);.
+
+//This will output the following:
+//{"version":"2.0.0","name":"Hypenexy"}
