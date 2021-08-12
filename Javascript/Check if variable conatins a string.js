be6ac7fabe176ca5605577eea9d20180d9c1b479
Midelight <61563710+Hypenexy@users.noreply.github.com>
2021-08-12 12:28:05 +0300

    Some javascript and fixed spaces

---
+++ Javascript/Check if variable conatins a string.js
@@ -0,0 +1,8 @@
+var somestring = "Hey there person, wait are you?";
+
+if(somestring.includes("person")){
+	console.log("omg it contains a person")
+}
+
+//It will return true if the string is found in the string.
+//It's also case sensitive!
