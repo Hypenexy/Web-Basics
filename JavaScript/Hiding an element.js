c25f53219514bbd7bba4c16d12da2478e0f805c5
Midelight <61563710+Hypenexy@users.noreply.github.com>
2021-08-14 23:46:52 +0300

    Delete JavaScript directory

--- JavaScript/Hiding an element.js
+++
@@ -1,7 +0,0 @@
-var element = document.getElementById("myEl");
-var elementtoappear = document.getElementById("myotherEl");
-
-element.style.display = "block";.
-
-/*And to make an element appear:*/
-elementtoappear.style.removeProperty = "display";
