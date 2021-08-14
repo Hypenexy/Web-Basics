67da7cb52171b13dbcf2322e46be34fe47e1a6b2
Midelight <61563710+Hypenexy@users.noreply.github.com>
2021-08-14 23:45:19 +0300

    Hiding stuff!

---
+++ JavaScript/Hiding an element.js
@@ -0,0 +1,7 @@
+var element = document.getElementById("myEl");
+var elementtoappear = document.getElementById("myotherEl");
+
+element.style.display = "block";.
+
+/*And to make an element appear:*/
+elementtoappear.style.removeProperty = "display";
