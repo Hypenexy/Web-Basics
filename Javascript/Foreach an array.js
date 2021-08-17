4159d6e74b39bc346af78ab0b666048244ca5e0c
Midelight <61563710+Hypenexy@users.noreply.github.com>
2021-08-17 03:02:56 +0300

    Update Foreach an array.js

--- Javascript/Foreach an array.js
+++ Javascript/Foreach an array.js
@@ -7,3 +7,16 @@ function somFunction(item){
 }
 
 //Should output: hey how are you 
+
+//Alternative method:
+
+fontAvailable = ["Arial", "Roboto"];
+
+function indexFonts(){
+  for (let item of fontAvailable){
+	console.log(item);
+  }
+}
+
+//Should output when called: ArialRoboto
+//You can also do it outside the function!
