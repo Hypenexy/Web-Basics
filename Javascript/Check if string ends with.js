da6fdf6f0d711eed5e38c42e1544a0150611fbf9
Midelight <61563710+Hypenexy@users.noreply.github.com>
2021-09-11 00:16:13 +0300

    Create Check if string ends with.js

---
+++ Javascript/Check if string ends with.js
@@ -0,0 +1,9 @@
+var filename = "coolfile.txt";
+
+if(filename.endsWith(".txt")){
+	console.log("Yep it's probably a txt file!")
+}
+
+// string.endsWith(anotherstring)
+
+// Will return true if it does and false if it doesn't.
