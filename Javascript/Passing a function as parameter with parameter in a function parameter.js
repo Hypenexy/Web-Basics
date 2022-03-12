ed665c70f729bb613d5cde3502ac0f8cd0857844
Midelight <61563710+Hypenexy@users.noreply.github.com>
2022-03-12 16:47:23 +0200

    Create Passing a function as parameter with parameter in a function parameter.js

---
+++ Javascript/Passing a function as parameter with parameter in a function parameter.js
@@ -0,0 +1,24 @@
+var buttons = welcome.getElementsByTagName("button")
+
+for (let i = 0; i < buttons.length; i++) {
+	ButtonEvent(buttons[i], function(){ return setLanguage(buttons[i].classList[0]) })
+}
+
+function ButtonEvent(element, action, param){
+    element.onclick = function(){
+        action(param)
+    }
+    element.onkeydown = function(e){
+        if(e.key == "Enter" || e.key == " "){
+            action(param)
+        }
+    }
+}
+
+
+
+//So in short, just
+
+buttonelement.onclick = function(){
+	SomeFunction("first parameter lol", function(){ return AnotherFunction(varlol); })
+}
