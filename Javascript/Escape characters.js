09478f91617d95b8ce65d834e861a3217e518df8
Midelight <61563710+Hypenexy@users.noreply.github.com>
2023-04-25 17:23:05 +0300

    Fixed Escape characters.js containing plainly wrong code

--- Javascript/Escape characters.js
+++ Javascript/Escape characters.js
@@ -1,6 +1,6 @@
 //To save: This says "Hello, World!"
 //inside another "" we must use escape characters.
-var txt = "This says \"Hello, World!\"
+var txt = "This says \"Hello, World!\""
 alert(txt);
 
 //Or simply.
@@ -10,3 +10,12 @@ alert(txt);
 //Which is almost the same as:
 var txt = 'This says "Hello, World!"';
 alert(txt);
+
+// Most recent way you can do it is to use `
+var txt = `It's a beautiful "world".`
+alert(txt)
+// You can even put in variables and new lines!
+var world = "world"
+var txt = `It's a
+beautiful ${world}.`
+alert(txt)
